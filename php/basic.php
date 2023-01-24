41aeced2f283702c7841e6d771ad970aed0b438f
Manibharathi <manibharath159@gmail.com>
2023-01-24 20:20:48 +0530

    Basic Php Added

---
+++ php/basic.php
@@ -0,0 +1,20 @@
+<?php
+echo "Hello World!\n";
+echo "hello\nWorld";
+echo "\n";
+$firstNumber = 10;
+$secondNumber = 20;
+echo $firstNumber + $secondNumber;
+
+$firstName = "Vijay";
+$secondName = "Raj";
+$result = $firstName.$secondName;
+echo $result;
+
+$mod = 10 % 3;
+echo $mod;
+
+$number = 10**2;
+
+echo "Number ".$number;
+
