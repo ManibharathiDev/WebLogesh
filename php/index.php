8089e1c4aa0edaad564ee9514c3cbfc9c2b1e0d7
Manibharathi <manibharath159@gmail.com>
2023-01-31 20:23:49 +0530

    loop added

--- php/index.php
+++ php/index.php
@@ -92,7 +92,7 @@
     // }
 
     //Write a php program to find greatest number between the three numbers.
-    //Write a php program to find greatest number between the four numbers
+    
      $a = 10;
      $b = 20;
      $c = 5;
@@ -104,16 +104,126 @@
 
      //$b or $c
 
-     if($a > $b && $a > $c)
-     {
-        echo "A is greatest number";
-     }
-     else if($b > $c)
-     {
-        echo "B is greatest number";
-     }
-     else{
-        echo "C is greatest number";
-     }
+   //   if($a > $b && $a > $c)
+   //   {
+   //      echo "A is greatest number";
+   //   }
+   //   else if($b > $c)
+   //   {
+   //      echo "B is greatest number";
+   //   }
+   //   else{
+   //      echo "C is greatest number";
+   //   }
+
+   //Write a php program to find greatest number between the four numbers
+
+   // $a = 10;
+   // $b = 20;
+   // $c = 35;
+   // $d = 5;
+   // if($a > $b && $a > $c && $a > $d)
+   // {
+   //    echo "A is greatest number";
+   // }
+   // else if($b > $c && $b > $d)
+   // {
+   //    echo "B is greatest number";
+   // }
+   // else if($c > $d)
+   // {
+   //    echo "C is greatest number";
+   // }
+   // else{
+   //    echo "d is greatest number";
+   // }
+
+   //Nested If-Else
+
+   // $a = 10;
+
+   // if($a > 5)
+   // {
+   //       if($a > 2)
+   //       {
+   //          echo "A is greatest number";
+   //          if(condition)
+   //          {
+
+   //          }
+   //       }
+   //       else{
+
+   //       }
+   // }
+   // else{
+
+   // }
+
+   // Write a program to find given number is odd or even number
+   // Odd Number : 1 3 5 7 9.....N
+   // Even Number : 2 4 6 8 10.....N
+    // $num = 18;
+    // $remainder = $num%2;
+    // if($remainder == 0)
+    //   echo("The number is even");
+    // else
+    //   echo ("The number is odd");
+
+   // Looping Statments - Iterations 
+  //  1. For Loop
+  //  2. While Loop
+  //  3. Do-While Loop
+
+  //Q.Write a php program to print 1 to 10 numbers;
+
+  // echo("1");
+  // echo("2");
+  // echo("3");
+  // echo("10");
+
+  //For Loop Syntax
+  // for(initializtion;condition;incre/decre)
+  // {
+  //     code block
+  // }
+    // $i++ = $i = $i+1
+    
+  // for($i = 1; $i <= 10; $i++)
+  // {
+  //     echo($i);
+  // }
+
+  // for($i = 1; $i <= 10; $i++)
+  // {
+  //     if($i%2 == 0)
+  //     {
+  //         echo($i." is odd number ");
+  //     }
+  // }
+    //print odd number
+  for($i = 2; $i <= 10; $i=$i+2)
+  {
+      echo ($i.",");
+  }
+
+  // print even number
+  for($i = 1; $i <= 10; $i=$i+2)
+  {
+      echo ($i.",");
+  }
+
+  //Find even numbers between 1 to 10
+    // 2,4,6,8,10
+
+
+
+
+    
+
+
+    
+
+
 
 ?>
---
+++ php/loop.php
@@ -0,0 +1,93 @@
+<?php
+// Looping Statments - Iterations 
+  //  1. For Loop
+  //  2. While Loop
+  //  3. Do-While Loop
+
+  //Q.Write a php program to print 1 to 10 numbers;
+
+  // echo("1");
+  // echo("2");
+  // echo("3");
+  // echo("10");
+
+  //For Loop Syntax
+  // for(initializtion;condition;incre/decre)
+  // {
+  //     code block
+  // }
+    // $i++ = $i = $i+1
+    
+  // for($i = 1; $i <= 10; $i++)
+  // {
+  //     echo($i);
+  // }
+
+  // for($i = 1; $i <= 10; $i++)
+  // {
+  //     if($i%2 == 0)
+  //     {
+  //         echo($i." is odd number ");
+  //     }
+  // }
+    //print odd number
+    for($i = 2; $i <= 10; $i=$i+2)
+    {
+        //echo ($i.",");
+    }
+  
+    // print even number
+    for($i = 1; $i <= 10; $i=$i+2)
+    {
+        //echo ($i."\n");
+    }
+
+    //Q. Print number between 10 to 1
+    for($i = 10; $i>=1; $i--)
+    {
+        //echo ($i.", ");
+    }
+
+    //While Loop
+    //Syntax
+    // intializtion;
+    // while(condition)
+    // {
+    //     true block;
+    //     inc/dec
+    // }
+    // Entry Level Checking    
+    $i = 1;
+    while($i<=10)
+    {
+        //echo($i." ");
+        $i++;
+    }
+
+    $i = 1;
+    while($i <=10)
+    {
+        if($i%2 == 0)
+        {
+           //echo($i." is odd number ");
+           
+        }
+        $i++;
+    }
+
+    //Do While
+    // Exit Level Checking
+    //Syntax
+    // initializtion
+    // do
+    // {
+    //     code block;
+    // }while(condition);
+
+    $i = 1;
+    do{
+        echo $i;
+        $i++;
+    }while($i>2);
+
+
